d6d128fba776ec1d7bd81a111c5580402cbc3f43
rizwanahmedit23 <102159323+rizwanahmedit23@users.noreply.github.com>
2022-08-24 01:37:43 +0530

    Update 1.Nth root of a Binary Number.cpp

--- Binary Search/1.Nth root of a Binary Number.cpp
+++ Binary Search/1.Nth root of a Binary Number.cpp
@@ -1,13 +1,17 @@
-double low = 1;
+double findNthRootOfM(int n, int m) {
+	// Write your code here.
+    double low = 0;
     double high = m;
-    double eps = 1e-6; 
+    double eps = 1e-7; 
     
     while((high - low) > eps) {
         double mid = (low + high) / 2.0; 
-        if(multiply(mid, n) < m) {
+        if(pow(mid, n) < (double)m) {
             low = mid; 
         }
         else {
             high = mid; 
         }
     }
+    return low;
+}
